be6cc57e9302c7128d1182912183e886fc26a465
william jose pires <williamjosepires@Mac-mini-de-william.local>
2018-11-04 16:43:25 -0200

    merge clientedao

---
+++ prjinfoX/src/br/com/infox/controller/ClienteController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.infox.controller;
+
+
+/**
+ *
+ * @author williamjosepires
+ */
+public class ClienteController {
+   
+    
+    public boolean pesquisarCliente(String pesuisar){
+        
+        
+         
+        
+        
+        
+        
+    }
+
+    
+   
+    
+  
+    
+    
+    
+    
+}
